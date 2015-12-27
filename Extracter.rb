efc0cc0c4d1f62f10696d6ad5f94ef0141be3b54
Norio <umanari145@gmail.com>
2015-12-27 20:21:18 +0900

    商品数を取得し、多ページに対応

--- Extracter.rb
+++ Extracter.rb
@@ -9,16 +9,28 @@ class Extracter
         doc = htmlGetter.getHtmlContents(topLink)
         #総アイテム数を取得
         itemCount = getItemCount( doc )
+        loopCnt = itemCount.to_i / 120
         
-        itemNodes = doc.xpath("//p[@class='tmb']" )
-        contentsLinks=[]
-       itemNodes.each{|childrenlist|
-           contentsLinks.push( childrenlist.css("a").attribute("href").value)
-       }
+        loopCnt=3
+        for i in 1..loopCnt do
+            
+            if i >= 2  then
+                url2 = topLink + "page="+ i.to_s 
+                doc = htmlGetter.getHtmlContents(url2) 
+            end
+            
+            itemNodes = doc.xpath("//p[@class='tmb']" )
+            
+            contentsLinks=[]
+            itemNodes.each{|childrenlist|
+               contentsLinks.push( childrenlist.css("a").attribute("href").value)
+            }
 
-       itemHashList = getItemHash( contentsLinks )
-        return itemHashList
+            itemHashList = getItemHash( contentsLinks )
+            p itemHashList
+        end
     end
+    
 
     def getItemCount( doc )
         itemCountElement = doc.xpath("//div[@class='list-boxcaptside list-boxpagenation'] //p").inner_text
@@ -77,8 +89,9 @@ class Extracter
         tds = itemHtml.xpath("//table[@class='mg-b20'] //td")
         itemHash = extractItemContents(tds)
         itemHash["largeImage"] = itemHtml.xpath("//div[@id='sample-video']").css("a").attribute("href").value
+        itemHash["contentsName"] = itemHtml.xpath("//h1[@id='title']").text
+        itemHash["summary"] = itemHtml.xpath("//div[@class='mg-b20 lh4']").text
         return itemHash
-        exit
     end
 
 end 
