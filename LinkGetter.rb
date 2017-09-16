71f7b334b6744768fbe9dfea4b3b48b365b91f35
Norio Mastsumoto <matsumoto@donow.jp>
2017-09-16 02:00:03 +0000

    リンクの抽出クラスを取得

--- LinkGetter.rb
+++ LinkGetter.rb
@@ -4,121 +4,69 @@ require './HtmlGetter'
 
 class LinkGetter
 
-    @topLink
+  @topLink
 
-    @itemCount
+  @itemCount
 
-    @perLoop
+  @perLoop
 
-    @LoopCount
+  @maxLoopCount
 
-    include HtmlGetter
+  include HtmlGetter
 
-    def initialize(topLink, perLoop)
-        @topLink = topLink
-        @perLoop = perLoop || 120
+  def initialize(topLink, perLoop)
+    @topLink = topLink
+    @perLoop = perLoop || 120
+  end
+
+  public def start()
+    doc = getHtmlContents(@topLink)
+    setLoopCount(doc)
+    allLinks = getLinks(doc)
+    return allLinks
+  end
+
+  private def setLoopCount(doc)
+    itemCount = getItemCount(doc)
+    @maxLoopCount = itemCount.to_i / @perLoop
+  end
+
+  def getItemCount( doc )
+    itemCountElement = doc.xpath("//div[@class='list-boxcaptside list-boxpagenation'] //p").inner_text
+    itemCountElement2 = itemCountElement.match(/^(.*?)タイトル中.*?$/)
+    pp itemCountElement2[1]
+    return itemCountElement2[1].delete(",")
+  end
+
+  private def getLinks(doc)
+    allLinks = []
+    @maxLoopCount = 2
+    for loopCount in 1..@maxLoopCount do
+      links = getLinkHtml(loopCount)
+      allLinks.concat(links)
     end
+    return allLinks
+  end
 
+  private def getLinkHtml(loopCount)
+    url = getEachUrl(loopCount)
+    links = getLinksEachSingleList(url)
+    return links
+  end
 
-    public def start()
-        doc = getHtmlContents(@topLink)
-        setLoopCount(doc)
-        links =  getLinks()
-    end
+  private def getEachUrl(loopCount)
+    sprintf "%{topLink}%{pager}%{pageNo}" , topLink:@topLink ,pager:'page=', pageNo:loopCount
+  end
 
-    private def setLoopCount(doc)
-        itemCount = getItemCount(doc)
-        @loopCount = itemCount.to_i / @perLoop
-    end
-
-    private def getLinks()
-
-
-    end
-        #総アイテム数を取得
-  #      itemCount = getItemCount( doc )
-  #      loopCnt = itemCount.to_i / 120
-  #
-  #      loopCnt=3
-  #      for i in 1..loopCnt do
-  #
-  #          if i >= 2  then
-  #              url2 = topLink + "page="+ i.to_s
-  #              doc = htmlGetter.getHtmlContents(url2)
-  #          end
-  #
-  #          itemNodes = doc.xpath("//p[@class='tmb']" )
-  #
-  #          contentsLinks=[]
-  #          itemNodes.each{|childrenlist|
-  #             contentsLinks.push( childrenlist.css("a").attribute("href").value)
-  #          }
-  #
-  #          itemHashList = getItemHash( contentsLinks )
-  #          p itemHashList
-  #      end
-
-    def getItemCount( doc )
-        itemCountElement = doc.xpath("//div[@class='list-boxcaptside list-boxpagenation'] //p").inner_text
-        itemCountElement2 = itemCountElement.match(/^(.*?)タイトル中.*?$/)
-        pp itemCountElement2[1]
-        return itemCountElement2[1].delete(",")
-    end
-
-
-    def getItemHash( contentsLinks )
-        itemHashList =[]
-        contentsLinks.each{|contentsLink|
-            itemHashList.push( makeItemHash( contentsLink ))
-        }
-        return itemHashList
-    end
-
-    def extractItemContents(tds)
-        stringTextMode=""
-        itemHash ={}
-
-        tds.each{|tdElement|
-            itemText = tdElement.text
-
-            if stringTextMode == "performer" then
-                itemHash[stringTextMode] = itemText
-                stringTextMode=""
-            elsif stringTextMode != "" then
-                itemHash[stringTextMode] = itemText.gsub(/(\r\n|\r|\n)/, "")
-                stringTextMode=""
-            end
-
-            if itemText.include?("配信開始日") then
-                stringTextMode="delivery_start"
-            elsif itemText.include?("対応デバイス") then
-                stringTextMode="device"
-            elsif itemText.include?("商品発売日") then
-                stringTextMode="release_date"
-            elsif itemText.include?("収録時間") then
-                stringTextMode="duration"
-            elsif itemText.include?("出演者") then
-                stringTextMode="performer"
-            elsif itemText.include?("シリーズ") then
-                stringTextMode="series"
-            elsif itemText.include?("ジャンル") then
-                stringTextMode="jenre"
-            elsif itemText.include?("品番") then
-                stringTextMode="product_code"
-            end
-        }
-        return itemHash
-    end
-
-    def makeItemHash (itemLink)
-        htmlGetter = HtmlGetter.new()
-        itemHtml = htmlGetter.getHtmlContents(itemLink)
-        tds = itemHtml.xpath("//table[@class='mg-b20'] //td")
-        itemHash = extractItemContents(tds)
-        itemHash["largeImage"] = itemHtml.xpath("//div[@id='sample-video']").css("a").attribute("href").value
-        itemHash["contentsName"] = itemHtml.xpath("//h1[@id='title']").text
-        itemHash["summary"] = itemHtml.xpath("//div[@class='mg-b20 lh4']").text
-        return itemHash
-    end
+  private def getLinksEachSingleList(url)
+    doc = getHtmlContents(url)
+    #スラッシュの前はスペースあける、
+    links = []
+    linkEles = doc.xpath('//div[@class="d-item"] //p[@class="tmb"]')
+    linkEles.each{|linkEle|
+      links.push(linkEle.css('a').attribute('href').text)
+    }
+    return links
+  end
 
 end
